88391730d129fe6706afed11a3944a09033c720f
Aluno <thalyaangel1704@gmail.com>
2019-04-29 12:16:50 -0300

    pipoca mais gostosa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  valor1: number = 0;
+  valor2: number = 0;
+  valor: number = 0;
+  somar (): vooid {
+    let total = Number(this.valor1) + Number(this.valor2);
+    alert("a soma é: "+total");
+    if (total %2 ==0){
+      alert ("é par")
+    }else {
+      alert ("é impar")
+    }
+  }
 
 }
